8bd6644b7e7dccd1966fafc4fe6bf952ccedf11a
maplanga <design2@maplanga.co.za>
2017-11-30 14:38:32 +0200

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -61,19 +61,14 @@ $$(document).on('deviceready', function() {
 });
 
 function checkConnection() {
-    var networkState = navigator.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+var networkstatus = 'No network connection';
 
-    myApp.alert('Connection type: ' + states[networkState]);
+	if (navigator.connection.type == NONE)
+	{
+		 myApp.alert('Connection type: ' + networkstatus);
+	}
+
 }
 
 checkConnection();
